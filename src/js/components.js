5ac94ad1c5d20d3a98538a44f781938c65f5e919
Facundo <facundoh1986@gmail.com>
2020-09-30 10:13:55 +0200

    Cambios Css

--- src/js/components.js
+++ src/js/components.js
@@ -4,7 +4,7 @@ export const saludar = (nombre, pais) => {
     console.log('creando etiqueta h1');
 
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${nombre} como estas? Todo bien por ${pais}?`; 
+    h1.innerText = `Hola, ${nombre} como estas?`;
 
     document.body.append(h1);
 }
